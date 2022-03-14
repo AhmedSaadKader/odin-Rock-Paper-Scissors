d6c49fab2486b52e00c3ada9ef8a9175d8560700
Ahmed Saad <ahmed.saad.kader@gmail.com>
2022-03-14 21:39:14 +0200

    function who wins "if..else" and game function "for loop"

--- scripts/script.js
+++ scripts/script.js
@@ -9,17 +9,33 @@ function computerPlay () {
 }
 
 function humanPlay () {
-    playerHand = prompt('What is your hand?')
+    playerHand = prompt('What is your hand?').toLowerCase()
     return playerHand
 }
 
-computerPlay()
-humanPlay()
+function playRound(playerHand, computerHand){
+    if (computerHand == 'rock' && playerHand == "paper"
+    || computerHand == "paper" && playerHand == "scissors"
+    || computerHand == "scissors" && playerHand == "rock") {
+        console.log(`You win: ${playerHand} beats ${computerHand}`)
+    } 
+    else if (computerHand == 'rock' && playerHand == "scissors"
+    ||       computerHand == 'paper' && playerHand == "rock"
+    ||       computerHand == 'scissors' && playerHand == "paper"){
+        console.log(`You lose: ${playerHand} loses to ${computerHand}`)
+    } 
+    else {
+        console.log("It's a tie!")
+    }
+}
 
+function game (){
+    for (let i = 0; i < 5; i++) {
+        computerPlay();        
+        humanPlay();
+        playRound(playerHand, computerHand);
+    }
+}
 
-if (computerHand == 'rock' && playerHand == "paper"
- || computerHand == "paper" && playerHand == "scissors"
- || computerHand == "scissors" && playerHand == "rock") {
-    alert(`You win: ${playerHand} beats ${computerHand}`)
-} 
-// else if (computerHand )
+game()
+
