a7b9bb6d3f9fdca9d6a193bf8a7f7a564b191da2
Ahmed Saad <ahmed.saad.kader@gmail.com>
2022-03-18 17:09:10 +0200

    added the logic to the DOM

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,29 @@
 const HAND = ['rock', 'paper', 'scissors']
-
-let playerHand, computerHand, playerScore = 0, computerScore = 0, playerSelection; 
+let playerHand, computerHand, playerScore = 0, computerScore = 0; 
+const rockButton = document.getElementById('rock')
+const paperButton = document.getElementById('paper')
+const scissorsButton = document.getElementById('scissors')
+const computerChoiceElement = document.getElementById('computer-choice')
+const playerChoiceElement = document.getElementById('player-choice')
+const outcomeElement = document.getElementById('outcome')
+const ScoreElement = document.getElementById('score')
 
 function computerPlay () {
     computerHand = HAND[Math.floor(Math.random ()*3)]
-    // console.log(computerHand)
+    computerChoiceElement.innerText = computerHand
     return computerHand
 }
 
-function humanPlay () {
-    playerHand = prompt('What is your hand?').toLowerCase()
-    
-    return playerHand, playerSelection
+function humanPlay (e) {
+    // playerHand = prompt('What is your hand?').toLowerCase()
+    // console.log(e.target.)
+    playerHand = e.target.innerText.toLowerCase()
+    playerChoiceElement.innerText = playerHand
+    return playerHand
+}
+
+function updateScorePlayerWin(playerHand, computerHand){
+
 }
 
 function playRound(playerHand, computerHand){
@@ -19,31 +31,35 @@ function playRound(playerHand, computerHand){
     || computerHand == "paper" && playerHand == "scissors"
     || computerHand == "scissors" && playerHand == "rock") {
         playerScore += 1;
-        console.log(`Computer played ${computerHand}`)
-        console.log(`You win: ${playerHand} beats ${computerHand}`)
-        console.log(`Score is Player:${playerScore}---Computer: ${computerScore}`)
+        outcomeElement.innerText = `Computer played ${computerHand}\nYou win: ${playerHand} beats ${computerHand}`
+        ScoreElement.innerText = `Score is Player:${playerScore}---Computer: ${computerScore}`
     } 
     else if (computerHand == 'rock' && playerHand == "scissors"
     ||       computerHand == 'paper' && playerHand == "rock"
     ||       computerHand == 'scissors' && playerHand == "paper"){
         computerScore += 1;
-        console.log(`Computer played ${computerHand}`)
-        console.log(`Score is Player:${playerScore}---Computer: ${computerScore}`)
-        console.log(`You lose: ${playerHand} loses to ${computerHand}`)
+        outcomeElement.innerText = `Computer played ${computerHand}\nYou lose: ${playerHand} loses to ${computerHand}`
+        ScoreElement.innerText = `Score is Player:${playerScore}---Computer: ${computerScore}`
     } 
     else {
-        console.log("It's a tie!")
+        outcomeElement.innerText = "It's a tie!"
     }
 }
 
-function game (){
-    for (let i = 0; i < 5; i++) {
+function game (playerHand){
+    // for (let i = 0; i < 5; i++) {
         computerPlay();        
-        humanPlay();
+        console.log(playerHand)
         // playerSelection = document.getElementById('playerHand').innerHTML = playerHand 
         playRound(playerHand, computerHand);
-    }
+    // }
 }
 
-game()
 
+
+rockButton.addEventListener('click', (e) => humanPlay(e))
+rockButton.addEventListener('click', () => game(playerHand))
+paperButton.addEventListener('click', (e) => humanPlay(e))
+paperButton.addEventListener('click', () => game(playerHand))
+scissorsButton.addEventListener('click', (e) => humanPlay(e))
+scissorsButton.addEventListener('click', () => game(playerHand))
